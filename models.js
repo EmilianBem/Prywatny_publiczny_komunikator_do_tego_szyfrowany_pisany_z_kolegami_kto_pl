faf59e8c805768b9b22b9c5d00b8974b7cb57aad
Kamil Janku <k.janku1@gmail.com>
2022-05-19 21:02:23 +0200

    bardzo duzo zmian kilka tygodni temu juz nei pamietam ale dziala i nie ruszac, bazowane na ss-auth

---
+++ models.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+module.exports.Uzytkownik = mongoose.model("Uzytkownik", new mongoose.Schema({
+  firstName:    { type: String, required: true },
+  lastName:     { type: String, required: true },
+  email:        { type: String, required: true, unique: true },
+  password:     { type: String, required: true },
+  photoUrl:     { type: String},
+}));
+
+module.exports.Konwersacja = mongoose.model("Konwersacja", new mongoose.Schema({
+  nazwa:   { type: String, required: true},
+  uczestnicy :    { type: Array, required: true }
+}));
+
+module.exports.Wiadomosc = mongoose.model("Wiadomosc", new mongoose.Schema({
+  autorId :    { type: String, required: true },
+  konwersacjaId :    { type: String, required: true },
+  tresc :    { type: String, required: true },
+  wyslano :    { type: String, required: true }
+}));
---
+++ routes/main.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const auth = require('../auth');
+
+let router = express.Router();
+
+/**
+ * Render the home page.
+ */
+router.get("/", (req, res) => {
+    res.render("index");
+});
+
+/**
+ * Render the dashboard page.
+ */
+router.get("/dashboard", auth.loginRequired, (req, res) => {
+    let userString = {
+        firstName: req.user.firstName,
+        lastName: req.user.lastName,
+        email: req.user.email
+    };
+
+    res.render("dashboard", { userString: JSON.stringify(userString, null, 2) });
+});
+
+module.exports = router;

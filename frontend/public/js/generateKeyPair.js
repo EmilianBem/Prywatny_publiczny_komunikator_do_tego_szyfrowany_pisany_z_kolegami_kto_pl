34599a7553f5ec7b0b8d5a17fec2194cdce6383e
Patryk Stefan Pniewski <patryk.s.pniewski@gmail.com>
2022-06-05 18:18:35 +0200

    fix aby js nie wykonywał się kilka razy

--- frontend/public/js/generateKeyPair.js
+++ frontend/public/js/generateKeyPair.js
@@ -1,8 +1,13 @@
-generateKeyPair().then((keys) => {
-    saveKey(keys.privateKey);
-    exportKey(keys.publicKey).then((key) => {
-        $("#publicKey")[0].value = JSON.stringify(key);
-    })
-});
+if (runGenerateKeyPair === undefined){
+    function runGenerateKeyPair(){
+        generateKeyPair().then((keys) => {
+            saveKey(keys.privateKey);
+            exportKey(keys.publicKey).then((key) => {
+                $("#publicKey")[0].value = JSON.stringify(key);
+            })
+        });
+    }
+    runGenerateKeyPair();
+}
 
 
--- frontend/public/js/nowaKonfa.js
+++ frontend/public/js/nowaKonfa.js
@@ -1,36 +1,39 @@
-fetch("http://localhost:4000/user", {
-    "method": "GET",
-    "mode": "cors",
-    "credentials": "include"
-})
-    .then(response => response.json()
-        .then(response => {
-            let userId = response.userId;
-            const socket = io("ws://localhost:4000");
+if (runNowaKonfa === undefined) {
+    function runNowaKonfa(){}
+    fetch("http://localhost:4000/user", {
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+    })
+        .then(response => response.json()
+            .then(response => {
+                let userId = response.userId;
+                const socket = io("ws://localhost:4000");
 
-            $('form').submit(() => {
-                let konfa = {};
-                konfa.nazwa = $('#nazwa').val();
-                if (konfa.nazwa === "")
-                    return;
-                konfa.uczestnicy = [userId, $('#odbiorcy').val()];
+                $('form').submit(() => {
+                    let konfa = {};
+                    konfa.nazwa = $('#nazwa').val();
+                    if (konfa.nazwa === "")
+                        return;
+                    konfa.uczestnicy = [userId, $('#odbiorcy').val()];
 
-                socket.emit('join-room', userId);
-                socket.emit('nowa-konfa', konfa, userId);
+                    socket.emit('join-room', userId);
+                    socket.emit('nowa-konfa', konfa, userId);
 
-                return false;
-            });
-
-            socket.on('konfa-redirect', (autor, room) => {
-                if (!room) {
-                    console.log('error: no room');
                     return false;
-                }
-                if (autor !== userId) return false;
-                window.location.href = '/messaging/?konfaId=' + room;
-            });
+                });
 
-        }))
-    .catch(err => {
-        console.log(err);
-    });
+                socket.on('konfa-redirect', (autor, room) => {
+                    if (!room) {
+                        console.log('error: no room');
+                        return false;
+                    }
+                    if (autor !== userId) return false;
+                    window.location.href = '/messaging/?konfaId=' + room;
+                });
+
+            }))
+        .catch(err => {
+            console.log(err);
+        });
+}
